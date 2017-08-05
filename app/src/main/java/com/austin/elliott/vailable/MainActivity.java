c3a1c79dde0a239b39bb91a3ec98d02e3ea5e7d8
Austin Cobbs <ajcobbs@yahoo.com>
2017-08-05 10:31:44 -0600

    initial commit

---
+++ app/src/main/java/com/austin/elliott/vailable/MainActivity.java
@@ -0,0 +1,13 @@
+package com.austin.elliott.vailable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
